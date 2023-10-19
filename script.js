1929a7563c970865b98b53559ba23067f29290da
smtrindade1 <146794394+smtrindade1@users.noreply.github.com>
2023-10-19 01:14:18 -0300

    formulario y back oscuro

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// Obtén una referencia al botón "Log In" y al formulario de registro
 const botonLogin = document.getElementById('login-button');
 const formularioRegistro = document.getElementById('registro-formulario');
-let formularioVisible = false;
+const overlay = document.getElementById('overlay');
 
-// Agrega un evento de clic al botón "Log In"
 botonLogin.addEventListener('click', () => {
-  // Cambia el estado del formulario (visible/oculto) al hacer clic en el botón "Log In"
-  formularioVisible = !formularioVisible;
-  
-  // Muestra u oculta el formulario de registro según el estado
-  formularioRegistro.style.display = formularioVisible ? 'block' : 'none';
+  // Agrega una clase al formulario para desplazarlo hacia abajo
+  formularioRegistro.style.top = '0'; // Cambia la propiedad 'top' para mostrar el formulario
+  overlay.style.display = 'block';
 });
-        
+
+// Agrega un evento de clic al overlay
+overlay.addEventListener('click', (e) => {
+  if (e.target === overlay) {
+    // Restablece la propiedad 'top' para ocultar el formulario
+    formularioRegistro.style.top = '-100%';
+    overlay.style.display = 'none';
+  }
+});
